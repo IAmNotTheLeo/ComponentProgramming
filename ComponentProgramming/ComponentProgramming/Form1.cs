cabcd7dd4da0cfa8e38618e54e946eb1b9892a1d
chiLee98 <56430392+chiLee98@users.noreply.github.com>
2020-01-21 14:44:56 +0000

    Start

---
+++ ComponentProgramming/ComponentProgramming/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Class_Library;
+
+namespace ComponentProgramming
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Width = 285;
+            this.Height = 400;
+            
+        }
+    }
+}
